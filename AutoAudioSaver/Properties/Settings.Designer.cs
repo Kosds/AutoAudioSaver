ce1f0dfa3c855d1a7c6a4c8b4593282c30a45609
Kosds <kos-dan@mail.ru>
2016-10-05 22:56:44 +0400

    Add new setting (downloadingPath)

--- AutoAudioSaver/Properties/Settings.Designer.cs
+++ AutoAudioSaver/Properties/Settings.Designer.cs
@@ -70,5 +70,17 @@ namespace AutoAudioSaver.Properties {
                 this["trackListWasSaved"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string downloadingPath {
+            get {
+                return ((string)(this["downloadingPath"]));
+            }
+            set {
+                this["downloadingPath"] = value;
+            }
+        }
     }
 }
